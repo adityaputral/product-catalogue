d2ff5210cefbd6a20381139ee9d3473193527055
Aditya Leksono <adityaputra7997@gmail.com>
2024-08-13 20:29:24 +0700

    add font awesome

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  css: ['~/assets/scss/main.scss'],
+  css: ['~/assets/scss/main.scss', "font-awesome/css/font-awesome.css"],
   vite: {
     css: {
       preprocessorOptions: {
